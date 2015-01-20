cea55bc920bb0439cc33ac1f328457ee8be6ee53
fengzhbo <563128453@qq.com>
2015-01-20 10:10:03 +0800

    完整测试

--- WCFTest.Client/Program.cs
+++ WCFTest.Client/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using WCFTest.Client.WCFService;
+using WCFTest.Contract.Interface;
 
 namespace WCFTest.Client
 {
@@ -10,6 +13,42 @@ namespace WCFTest.Client
     {
         static void Main(string[] args)
         {
+
+            GetDealerInfo();
+
+
+
+
+            //  Console.WriteLine(info.Result.DealerShortName);
+
+            //info.GetAwaiter().OnCompleted(() =>
+            //{
+            //    Console.WriteLine(info.Result.DealerShortName);
+            //});
+
+
+            //using (ChannelFactory<IDealers> factory = new ChannelFactory<IDealers>("WSHttpBinding_DeslersService"))
+            //{
+
+            //    var proxy = factory.CreateChannel();
+
+            //    Console.WriteLine(proxy.GetDealer(50002218).DealerShortName);
+
+            //}
+        }
+
+        private static async void GetDealerInfo()
+        {
+            using (DeslersServiceClient client = new DeslersServiceClient())
+            {
+                var info = client.GetDealerAsync(5002218);
+
+               // Console.WriteLine(info.Result.DealerShortName);
+
+                await info;
+
+                Console.WriteLine(info.Result.DealerShortName);
+            }
         }
     }
 }
---
+++ WCFTest.Contract/DealerInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+
+namespace WCFTest.Contract.Model
+{
+    [DataContract]
+    public class DealerInfo
+    {
+        [DataMember]
+        public Int32 DealerID { get; set; }
+
+        [DataMember]
+        public String DealerShortName { get; set; }
+
+
+    }
+}
